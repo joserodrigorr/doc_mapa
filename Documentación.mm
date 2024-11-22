6078ff95e9d54aa74b1af473f15e307e44463fa6
José Rodrigo Roldán R. I&S <98901896+ISJoseRoldan@users.noreply.github.com>
2024-11-22 16:26:46 -0500

    Actualizacion inicial
    Actualizacion inicial

---
+++ Documentación.mm
@@ -0,0 +1,113 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1732304408093" ID="ID_254632916" MODIFIED="1732306609971" TEXT="Documentaci&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node COLOR="#0033ff" CREATED="1732304435313" ID="ID_1146058416" MODIFIED="1732305408209" POSITION="right" TEXT="Suite Connekta">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1732304591334" ID="ID_61522731" MODIFIED="1732305408219" TEXT="Gu&#xed;as">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#0066ff" CREATED="1732304634750" ID="ID_1446765154" MODIFIED="1732309710898" TEXT="Gesti&#xf3;n Usuarios y Permisos">
+<edge COLOR="#000000"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1732305706725" ID="ID_1639019189" MODIFIED="1732305943696" TEXT="Descripci&#xf3;n">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1732305731946" ID="ID_1043648577" MODIFIED="1732305734058" TEXT="Se describe a continuaci&#xf3;n como se gestionan los usuarios y permisos en el Sistema de Integraci&#xf3;n de Siesa, con alcance para el M&#xf3;dulo de Conectividad y el M&#xf3;dulo de APIs Din&#xe1;micas con aplicaci&#xf3;n en los roles de usuario tipo &#x201c;Admin&#x201d; y &#x201c;Cliente&#x201d;. "/>
+</node>
+<node COLOR="#111111" CREATED="1732305786923" ID="ID_1568891234" MODIFIED="1732305952559" TEXT="Ingreso al Gestor de Integraciones">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1732305979776" ID="ID_653584829" MODIFIED="1732305981716" TEXT="Para ingresar al Gestor de Integraciones es necesario disponer y usar un enlace (Link) similar al siguiente para su compa&#xf1;&#xed;a; uno para QA (Ambiente de pruebas) y otro para CORE (Ambiente de Producci&#xf3;n), en esta gu&#xed;a nos basaremos solo en la parte de QA, pero es igual para CORE, la diferencia entre QA y CORE es que en QA se trabaja en ambiente de pruebas y en CORE se trabaja en ambiente productivo en el ERP de Siesa.  "/>
+</node>
+<node CREATED="1732306064275" ID="ID_1097022912" MODIFIED="1732306845923" TEXT="URL para QA: ">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1732306089873" ID="ID_1059249499" MODIFIED="1732306797726" TEXT="https://integradorqa.siesacloud.com/login/[nombrecompa&#xf1;ia] &#xa; &#xa;Donde [nombrecompa&#xf1;ia] se reemplaza con el nombre asignado a su compa&#xf1;&#xed;a, para el ejemplo usaremos la siguiente: &#xa;&#xa;https://integradorqa.siesacloud.com/login/generictransfer &#xa;&#xa;Al ingresar a este enlace en un navegador web se debe mostrar algo similar a la siguiente imagen: "/>
+<node CREATED="1732306136895" ID="ID_416226196" MODIFIED="1732306566909" TEXT="...">
+<node CREATED="1732306519646" ID="ID_1485894533" MODIFIED="1732306542284">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Imagenes/1_Gestor_Integraciones.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1732306808174" ID="ID_1989820593" MODIFIED="1732306835091" TEXT="Dependiendo de lo adquirido por la compa&#xf1;&#xed;a donde aparece la etiqueta &#x201c;Gestor de Integraciones&#x201d; podr&#xed;a aparecer tambi&#xe9;n la etiqueta &#x201c;Modulo de Conectividad&#x201d; en vez de la etiqueta anterior.      Para ingresar debe disponer de un usuario (por lo general correo electr&#xf3;nico)   y una contrase&#xf1;a valida, credenciales asignadas por el administrador del Gestor de Integraciones, al ingresar las credenciales validas, se muestra el siguiente formulario: "/>
+<node CREATED="1732306856653" ID="ID_640977710" MODIFIED="1732306863193" TEXT="...">
+<node CREATED="1732306944366" ID="ID_572218942" MODIFIED="1732306955697">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Imagenes/2_MS_Gestor_integraciones.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1732306976823" ID="ID_1579689643" MODIFIED="1732306979469" TEXT="Al iniciar se muestran diferentes datos estad&#xed;sticos del Gestor de integraciones.      En esta gu&#xed;a nos centraremos en la Opci&#xf3;n Administraci&#xf3;n, y dentro de esta en las opciones Usuarios y Permisos.      Al presionar la opci&#xf3;n &#x201c;Administraci&#xf3;n&#x201d;, la primera opci&#xf3;n en la parte superior a la izquierda nos aparece el siguiente formulario "/>
+</node>
+<node CREATED="1732307006710" ID="ID_1997938842" MODIFIED="1732307096941" TEXT="Administraci&#xf3;n ">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1732307100982" ID="ID_1930096169" MODIFIED="1732307107503" TEXT="...">
+<node CREATED="1732307107503" ID="ID_1116505942" MODIFIED="1732307173127">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Imagenes/3_MS_Administracion.png" />
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1732307181927" ID="ID_604721844" MODIFIED="1732307199159" TEXT="En esta gu&#xed;a inicialmente solo describiremos el funcionamiento del manejo de las opciones Usuarios y Permisos "/>
+</node>
+</node>
+<node COLOR="#0066ff" CREATED="1732304444312" ID="ID_450743678" MODIFIED="1732307586831" TEXT="M&#xf3;dulo de Conectividad">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#0033ff" CREATED="1732304466663" ID="ID_280331832" MODIFIED="1732307599123" TEXT="M&#xf3;dulo APIs Din&#xe1;micas">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1732304526822" ID="ID_785603137" MODIFIED="1732305408219" POSITION="right" TEXT="Ecommerce">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1732304550303" ID="ID_1618203580" MODIFIED="1732305408219" TEXT="VTEX">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1732304553703" ID="ID_1382745306" MODIFIED="1732305408219" TEXT="Shopify">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1732304558166" ID="ID_690195426" MODIFIED="1732305408219" TEXT="Woocommerce">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1732304568360" ID="ID_1269905919" MODIFIED="1732305408219" TEXT="Didi">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1732304532831" ID="ID_1316040953" MODIFIED="1732305408219" POSITION="right" TEXT="VMS">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1732304541343" ID="ID_1431193835" MODIFIED="1732305408219" POSITION="right" TEXT="MIX">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1732304514783" ID="ID_614850495" MODIFIED="1732305408219" POSITION="right" TEXT="Siesa Expenses">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
